ea27f29a5d7dc5922bbc2a2f1a406c3fedc6703e
Friendly PMNS <friendlypmnsprimes@gmail.com>
2024-10-31 16:57:40 +0100

    Update EXAMPLE.py

--- pmns_generator/EXAMPLE.py
+++ pmns_generator/EXAMPLE.py
@@ -11,23 +11,27 @@ lv_tolerance = -1
 
 double_spare = True	#'True' for DoubleSparse PMNS or 'False' for LinearRed PMNS
 
-alpha_max = 4	#maximum absolute value allowed for 'alpha'
+alpha_max = 8	#maximum absolute value allowed for 'alpha'
 
-lambda_max = 4	#maximum absolute value allowed for 'lambda'
+lambda_max = 8	#maximum absolute value allowed for 'lambda'
 
 delta = 0		#the (minimum) number of desired "free" additions before a modular multplication  
 
-n = (p_size//phi_log2) + 1	#should be incremented (if the generator takes too long to find a PMNS) as much as necessary, especially if "delta > 0" 
+n = (p_size//phi_log2) + 1	#IMPORTANT: if the generator takes too long to find a PMNS, 'n' should be incremented as much as necessary, especially if "delta > 0" 
 
 nb_pmns = 6	#the desired number of PMNS (if any)
 
 
-p_list = look_for_good_primes(p_size, lv_tolerance, n, phi_log2, delta, alpha_max, lambda_max, double_spare, nb_pmns)	#THE PMNS GENERATOR
+pmns_list = look_for_good_primes(p_size, lv_tolerance, n, phi_log2, delta, alpha_max, lambda_max, double_spare, nb_pmns)	#THE PMNS GENERATOR
 
 
 #Note (result struct): [p.nbits(), p, n, str(E), t, phi_log2, delta]
 
 #Extended parameters can be chosen/uncommented in the function 'ckeck_poly' 
+
+
+for pmns in pmns_list:
+	print(f'{pmns}\n')
 
 
 #-----------------------------------------------------------------------
